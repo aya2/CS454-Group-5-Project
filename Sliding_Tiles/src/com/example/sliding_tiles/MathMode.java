ca86a6712b578623bdc91f503e11bb617d25ba74
Michael Reames <reames.michael@gmail.com>
2012-11-13 11:13:56 -0800

    mathmode gui stuff

--- Sliding_Tiles/src/com/example/sliding_tiles/MathMode.java
+++ Sliding_Tiles/src/com/example/sliding_tiles/MathMode.java
@@ -1,16 +1,40 @@
 package com.example.sliding_tiles;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.res.Resources;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.TextView;
 
-public class MathMode extends Activity 
+public class MathMode extends Activity implements OnGesturePerformedListener	
 {
 
+	//gesture library
+	private GestureLibrary gestureLibrary;
+
+	//gesture listeners
+	private GestureDetector gestureDetector;
+
+	//textView for score
+	private TextView score;
+	
+	//textView for message
+	private TextView validMessage;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
@@ -26,12 +50,30 @@ public class MathMode extends Activity
 		//set the adapter for the gridview
 		gridview.setAdapter(mathadapter);
 
+		//setup the textView for the score
+		Resources res = getResources(); 
+		Drawable shape = res.getDrawable(R.drawable.rounded);
+		
+		//TextView tv = (TextView)findViewById(R.id.textView3);
+		score = (TextView) findViewById(R.id.textView3);
+		score.setBackgroundDrawable(shape);
+		
+		//set up the textView for the valid/invalid message
+		validMessage = (TextView)findViewById(R.id.textView1);
+		//set to invisible
+		validMessage.setVisibility(validMessage.INVISIBLE);
+		
 		//set the on-click listener for the gridview
 		gridview.setOnItemClickListener(new OnItemClickListener()
 		{
 			//when a tile is clicked
 			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
 			{
+				//Toast.makeText(v.getContext(),"position: " + position + " has been clicked", Toast.LENGTH_LONG).show();
+				
+				//hide the valid message
+				validMessage.setVisibility(validMessage.INVISIBLE);
+				
 				//call the update board function to check and see if it is a legal move and move if so
 				mathadapter.updateBoard(position);
 				
@@ -40,8 +82,15 @@ public class MathMode extends Activity
 			}
 		});
 
+		gestureLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+		if(!gestureLibrary.load())
+			finish();
 
-	}
+		//gesture overlay
+		GestureOverlayView gestures = (GestureOverlayView)findViewById(R.id.gestures);
+		gestures.addOnGesturePerformedListener(this);
+
+	}	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
@@ -50,5 +99,24 @@ public class MathMode extends Activity
 		return true;
 	}
 
+	//handle the gestures
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture)
+	{
+		ArrayList<Prediction> predictions = gestureLibrary.recognize(gesture);
+		for (Prediction prediction : predictions)
+		{
+			if (prediction.score > 8)//(predictions.size() > 0 && predictions.get(0).score > 4.0) 
+			{
+				//String result = prediction.name;
+				//Toast.makeText(this, result + "score =  " + prediction.score, Toast.LENGTH_LONG).show();
+				
+				//show the message
+				validMessage.setVisibility(validMessage.VISIBLE);
+				
+				//update the score
+				score.setText("13");
+			}
+		}
+	}
 
 }
